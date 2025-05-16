1bd9bf55ab34c77d3d0e4b650bb7e5f42ffba7eb
Celia <celia@MacBookPro.lan>
2025-05-16 14:06:03 -0700

    deploy

---
+++ node_modules/.cache/gh-pages/https!github.com!xinranyu33!25_Z_Bday.git/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  
+  theme: {
+    
+    extend: {
+      fontFamily: {
+      // hand: ['"Patrick Hand"', 'cursive'],
+      hand: ['"Gloria Hallelujah"', 'cursive'],
+      
+      }
+      
+    },
+  },
+  plugins: [],
+  
+}
